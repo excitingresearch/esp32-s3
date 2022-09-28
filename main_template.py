6bc6f27ebc568a64adc7902a844ee1972f565162
nversbra <nversbra@icloud.com>
2022-09-28 16:57:07 +0200

    added functionality

--- main_template.py
+++ main_template.py
@@ -3,14 +3,12 @@ from machine import Pin, Timer
 import time
 import network
 
-
 import ubluetooth
 import bluetooth
 
 import random
 import struct
 import webrepl
-
 
 from ble_advertising import advertising_payload
 
@@ -20,23 +18,17 @@ import mlx90615
 
 
 
-    
-
-
-
 try:
-
-
 
     _IRQ_CENTRAL_CONNECT = const(1)
     _IRQ_CENTRAL_DISCONNECT = const(2)
     _IRQ_GATTS_WRITE = const(3)
-    
+
     _FLAG_READ = const(0x0002)
     _FLAG_WRITE_NO_RESPONSE = const(0x0004)
     _FLAG_WRITE = const(0x0008)
     _FLAG_NOTIFY = const(0x0010)
-    
+
     _UART_UUID = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
     _UART_TX = (
         bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"),
@@ -50,14 +42,20 @@ try:
         _UART_UUID,
         (_UART_TX, _UART_RX),
     )
-    
-    
-    class BLESimplePeripheral:
-        def __init__(self, ble, name="moody_001"):
 
-            self.i2c = machine.SoftI2C(sda=machine.Pin(1, pull=machine.Pin.PULL_UP), scl=machine.Pin(2, pull=machine.Pin.PULL_UP), freq=100000)   
+
+    class BLESimplePeripheral:
+        def __init__(self, ble, name="moody_xxx"):
+
+            self.i2c = machine.SoftI2C(sda=machine.Pin(1, pull=machine.Pin.PULL_UP),
+                                       scl=machine.Pin(2, pull=machine.Pin.PULL_UP), freq=100000)
             self.irsensor = mlx90615.MLX90615(self.i2c)
 
+            self.temps = []
+
+            self.update_interval = 1000
+            self.brightness_mult = 100
+            self._sa = False
             self.np = neopixel.NeoPixel(machine.Pin(42), 1)
             self._ble = ble
             self._ble.active(True)
@@ -66,25 +64,27 @@ try:
             self._connections = set()
             self._write_callback = self.on_write
             self._payload = advertising_payload(name="moody",
-             services=[_UART_UUID])
+                                                services=[_UART_UUID])
             self._advertise()
             self._ble.config(gap_name=name)
-            self._set_pixel((0,255,0))
+            self._set_pixel((0, 255, 0))
             self.connect_wifi()
 
-        
-        def _set_pixel(self,c):
+
+        def _set_pixel(self, c):
+            c = [int(e * self.brightness_mult/100) for e in c]
+            print(c)
             self.np[0] = c
-            self.np.write()    
+            self.np.write()
 
         def _get_pixel(self):
             c = self.np[0]
-            return c        
-    
+            return c
+
         def _irq(self, event, data):
             # Track connections so we can send notifications.
             if event == _IRQ_CENTRAL_CONNECT:
-                self._set_pixel((0,0,255))
+                self._set_pixel((0, 0, 255))
                 conn_handle, _, _ = data
                 print("New connection", conn_handle)
                 self._connections.add(conn_handle)
@@ -99,29 +99,44 @@ try:
                 value = self._ble.gatts_read(value_handle)
                 if value_handle == self._handle_rx and self._write_callback:
                     self._write_callback(value)
-    
+
         def send(self, data):
             for conn_handle in self._connections:
                 self._ble.gatts_notify(conn_handle, self._handle_tx, data)
-    
+
         def is_connected(self):
             return len(self._connections) > 0
-    
+
         def _advertise(self, interval_us=500000):
             print("Starting advertising")
             self._ble.gap_advertise(interval_us, adv_data=self._payload)
-    
+
         def on_write(self, v):
-            msg_type = int(v[:1],16)
-            if msg_type == 1 :
-                self._set_pixel((int(v[1:3],16),int(v[3:5],16), int(v[5:7],16)))
+            msg_type = int(v[:1], 16)
+            if msg_type == 1:
+                self._set_pixel((int(v[1:3], 16), int(v[3:5], 16), int(v[5:7], 16)))
                 print("RX", v)
             elif msg_type == 2:
-                self._set_pixel((0,0,0))
-                print("going to sleep for "+ str(int(v[1:],16))+ "microsecs")
-                machine.deepsleep(int(v[1:],16))    
-            #self._write_callback = callback
-
+                up_i = int(v[1:], 16)
+                up_i = max(1, up_i)
+                print("Update interval set to: ", up_i)
+                self.update_interval = up_i
+            elif msg_type == 3:
+                bright = int(v[1:], 16) 
+                bright = max(1, bright)
+                bright = min(100, bright)
+                print("Brightness set to: ", bright)
+                self.brightness_mult = bright
+            elif msg_type == 4:
+                self._set_pixel((0, 0, 0))
+                print("going to sleep for " + str(int(v[1:], 16)) + "microsecs")
+                machine.deepsleep(int(v[1:], 16))
+                # self._write_callback = callback
+            elif msg_type == 5:
+                self._sa = not self._sa
+                print("Stand alone mode active: " + str(self._sa))
+            else: 
+                print("Unknown message: ", str(v))    
 
         def check_wifi(self):
             sta_if = network.WLAN(network.STA_IF)
@@ -132,11 +147,17 @@ try:
                 if len(e[0]) > 0 and e[0].decode('utf-8') == 'Moody-net':
                     moody_net_av = True
             if moody_net_av:
-                print("Moody-net available")        
+                print("Moody-net available")
             else:
-                print("Moody-net not available")            
-            return moody_net_av        
-        
+                print("Moody-net not available")
+            return moody_net_av
+
+        def blink(self, c):
+            self._set_pixel(c)
+            time.sleep_ms(100)
+            self._set_pixel((0, 0, 0))
+            time.sleep_ms(100)
+
         def connect_wifi(self):
             ssid = 'Moody-net'
             pwd = 'ExcitingResearch'
@@ -146,54 +167,107 @@ try:
                     print('connecting to network...')
                     sta_if.active(True)
                     sta_if.connect(ssid, pwd)
-                    while not sta_if.isconnected():
-                        pass
-                    print('network config:', sta_if.ifconfig())
-                    webrepl.start()
-                    c_col = self._get_pixel()
-                    for _ in range(5):
-                        self._set_pixel((255,20,147))
-                        time.sleep_ms(100)
-                        self._set_pixel((0,0,0))
-                        time.sleep_ms(100)
-                    self._set_pixel(c_col)    
+                    time.sleep_ms(10000)
+                    if sta_if.isconnected():
+                        print('network config:', sta_if.ifconfig())
+                        webrepl.start()
+                        c_col = self._get_pixel()
+                        wifi_c_pink = (255, 20, 147)
+                        for _ in range(5):
+                            self.blink(wifi_c_pink)
+                        self._set_pixel(c_col)
+                    else:
+                        c_col = self._get_pixel()
+                        wifi_c_f = (255, 0, 0)
+                        for _ in range(5):
+                            self.blink(wifi_c_f)
+                        self._set_pixel(c_col)   
+                        print("Unable to connect to wifi, error with Moody-net")
+
                 else:
-                    print("Unable to connect to wifi, no Moody-net")    
-            
-    
-    
+                    print("Unable to connect to wifi, no Moody-net")
+
+        def calc_color(self, result):
+            result = result/100
+            self.temps.append(result)
+            if len(self.temps) > 5:
+                
+                if len(self.temps) > 100:
+                    self.temps = self.temps[1:]
+                avgTemp = sum(self.temps) / len(self.temps)
+                max_temp = max(self.temps)
+                min_temp = min(self.temps)
+                delta = result - avgTemp
+                if abs(delta) < 0.1:
+                    c = self._get_pixel()
+                ## same
+                #print("delta: ", delta, end=" ")
+                elif delta < 0:
+                    ## down
+                    spr = avgTemp - min_temp
+                    dev = -1*delta / spr
+                    #print("downspread: ", spr, end=" ")
+                    #print("downdev: ", dev)
+                    cb = 255 * dev
+                    cg = 255 * (1 - dev)
+                    cb = max(0, cb)
+                    cg = max(0, cg)
+                    c = (0, int(cg), int(cb))
+                elif delta > 0:
+                    ## up
+                    spr = max_temp - avgTemp
+                    dev = delta / spr
+                    #print("upspread: ", spr, end=" ")
+                    #print("updev: ", dev)
+                    cr = 255 *  dev
+                    cg = 255 * (1- dev)
+                    cr = max(0, cr)
+                    cg = max(0, cg)
+                    #print("uprgb: ", cr, " ", cg, " ", 0)
+                    c = (int(cr), int(cg), 0)
+                self._set_pixel(c)
+
+
     def demo():
         ble = bluetooth.BLE()
         p = BLESimplePeripheral(ble)
-    
-        #def on_rx(v):
-        #    print("RX", v)
-    
-        #p.on_write(on_rx)
-    
-        t_val = 0
 
-            
+        t_val = 0
+        
         while True:
-            if p.is_connected():
-                # Short burst of queued notifications.
-                try:
+            if p._sa:
+                try:                
                     t_val = p.irsensor.read_object_temp()
+                    p.calc_color(t_val)
+                    time.sleep_ms(p.update_interval)
                 except Exception as e:
                     print (e)
-                data = str(t_val)
-                print("TX", data)
-                p.send(data)
-            time.sleep_ms(1000)
-    
-    demo()    
-    
-    
 
-    
-    
+            else:
+                if p.is_connected():
+                    try:                
+                        t_val = p.irsensor.read_object_temp()
+                        #p.calc_color(t_val)                        ## Don't foget to remove this 
+                        data = str(t_val)
+                        print("TX", data)
+                        p.send(data)
+                    except Exception as e:
+                        print (e)
+
+                time.sleep_ms(p.update_interval)
+# try:                
+# except Exception as e:
+#    print (e)
+
+
+    demo()
+
+
+
+
+
 except Exception as e:
     print(e)
-    
- 
+
+
 
